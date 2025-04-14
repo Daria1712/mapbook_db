aa260f2b7dc08a8b6b0ab89740a8f110f61856b3
Daria1712 <daria.borowska1712@gmail.com>
2025-04-14 08:56:44 +0200

    Wip: new menu option: remove users

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from utils.controller import get_user_info, add_user
+from utils.controller import get_user_info, add_user, remove_user
 from utils.model import users
+
 
 
 def main():
@@ -8,6 +9,7 @@ def main():
         print('0 - Exit')
         print('1-Get user info')
         print('2 - Add user')
+        print('3 - Remove user')
         print('=========================================')
 
 
@@ -15,6 +17,7 @@ def main():
         if choice == '0': break
         if choice == '1': get_user_info(users)
         if choice == '2': add_user(users)
+        if choice == '3': remove_user(users)
 
 if __name__ == '__main__':
     main()
--- utils/controller.py
+++ utils/controller.py
@@ -7,3 +7,9 @@ def add_user(user_data: list[dict]) -> None:
     tmp_Location:str = input('Podaj miejsce: ')
     tmp_Posts:int = int(input('Podaj liczbę postów: '))
     user_data.append({'Name': tmp_Name, 'Location': tmp_Location, 'Posts': int(tmp_Posts)})
+
+def remove_user(users_data: list[dict])-> None:
+    user_Name=input('Podaj imię użytkownika do usunięcia: ')
+    for user in users_data:
+        if user['Name']==user_Name:
+            users_data.remove(user)
