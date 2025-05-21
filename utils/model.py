8138af269d7a70efffd110f32e6f0a4132be4b6d
Daria1712 <daria.borowska1712@gmail.com>
2025-05-21 08:42:09 +0200

    init: start empty project

--- utils/model.py
+++
@@ -1,5 +0,0 @@
-users: list[dict] = [
-    {'Name': 'Kinga', 'Location': 'Warszawa', 'Posts': 500, },
-    # {'Name': 'Wika', 'Location': 'Radzyń', 'Posts': 400, },
-    # {'Name': 'Daria', 'Location': 'Łosice', 'Posts': 200, }
-]
